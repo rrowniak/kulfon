5183474c57b00626f2be83b8a620a98f55cdca2d
Rafal Rowniak <rrowniak@gmail.com>
2024-07-20 10:11:16 +0200

    Added function lookup in kf type system

--- kf/src/kf_core.rs
+++ kf/src/kf_core.rs
@@ -8,9 +8,11 @@ use crate::ast;
 use crate::lang_def::ParsingError;
 use crate::lang_def::TextPoint;
 use crate::type_system::*;
+use std::collections::HashMap;
 
 pub struct Context {
     pub side_nodes: Vec<SideNode>,
+    pub glob_functs: HashMap<String, (Vec<ast::VarDecl>, ast::TypeDecl)>,
 }
 
 pub struct InterRepr {
@@ -22,6 +24,7 @@ impl InterRepr {
     pub fn from(mut syntree: ast::Node) -> Result<Self, Vec<ParsingError>> {
         let mut ctx = Context {
             side_nodes: Vec::new(),
+            glob_functs: HashMap::new(),
         };
         first_pass(&mut syntree, &mut ctx)?;
         second_pass(&mut syntree, &mut ctx)?;
@@ -31,6 +34,7 @@ impl InterRepr {
 
 fn first_pass(syntree: &mut ast::Node, ctx: &mut Context) -> Result<(), Vec<ParsingError>> {
     // collect all function definitions
+    collect_glob_functions(syntree, ctx)?;
     // evaluate expression types
     eval_types(syntree, ctx)?;
     Ok(())
@@ -38,6 +42,56 @@ fn first_pass(syntree: &mut ast::Node, ctx: &mut Context) -> Result<(), Vec<Pars
 
 fn second_pass(syntree: &mut ast::Node, ctx: &mut Context) -> Result<(), Vec<ParsingError>> {
     Ok(())
+}
+
+fn collect_glob_functions(
+    syntree: &mut ast::Node,
+    ctx: &mut Context,
+) -> Result<(), Vec<ParsingError>> {
+    let mut errors = Vec::new();
+    match &mut syntree.val {
+        ast::Ntype::Scope(scope) => {
+            for st in scope.iter_mut() {
+                collect_glob_functions(st, ctx)?;
+            }
+        }
+        ast::Ntype::FnDef(fndef) => {
+            let ast::Fun {
+                name,
+                args,
+                ret,
+                body: _,
+            } = fndef;
+            // check if the name is correct
+            if let Some(c) = name.chars().next() {
+                if !c.is_ascii_alphabetic() {
+                    errors.push(ParsingError {
+                        msg: "function name should start with alphabetic ascii character".into(),
+                        details: format!("first character >{c}< is not allowed in function name"),
+                        at: syntree.at,
+                    });
+                }
+            }
+            if !ctx
+                .glob_functs
+                .insert(name.clone(), (args.clone(), ret.clone()))
+                .is_none()
+            {
+                // collision - this function already defined
+                errors.push(ParsingError {
+                    msg: "function with this name already defined".into(),
+                    details: "function name should be unique across module scope".into(),
+                    at: syntree.at,
+                })
+            }
+        }
+        _ => {}
+    }
+    if errors.len() > 0 {
+        Err(errors)
+    } else {
+        Ok(())
+    }
 }
 
 macro_rules! calculate_operator {
@@ -92,16 +146,17 @@ fn eval_types(n: &mut ast::Node, s: &mut Context) -> Result<(), Vec<ParsingError
             eval_types(a.as_mut(), s)?;
             eval_types(b.as_mut(), s)?;
             let _t = determine_type_for_a_b(&a, &b, n.at, s)?;
-            let val = calculate_operator!(
-                get_side_node(&a, s).unwrap().eval_val.clone().unwrap(),
-                ==,
-                get_side_node(&b, s).unwrap().eval_val.clone().unwrap()
-            );
-            let val = if let Some(v) = val {
-                Some(EvaluatedValue::Bool(v))
-            } else {
-                None
-            };
+            let val = None;
+            // let val = calculate_operator!(
+            //     get_side_node(&a, s).unwrap().eval_val.clone().unwrap(),
+            //     ==,
+            //     get_side_node(&b, s).unwrap().eval_val.clone().unwrap()
+            // );
+            // let val = if let Some(v) = val {
+            //     Some(EvaluatedValue::Bool(v))
+            // } else {
+            //     None
+            // };
             set_type(
                 n,
                 s,
@@ -242,6 +297,7 @@ fn eval_types(n: &mut ast::Node, s: &mut Context) -> Result<(), Vec<ParsingError
         ast::Ntype::Assign(a, b) => {
             // TODO: a is a literal, lookup type and check if rhs == lhs
             eval_types(b.as_mut(), s)?;
+            set_type_void(n, s);
         }
         // control flow
         ast::Ntype::If(if_) => {
@@ -284,11 +340,15 @@ fn eval_types(n: &mut ast::Node, s: &mut Context) -> Result<(), Vec<ParsingError
             if errors.len() > 0 {
                 return Err(errors);
             }
+            // TODO: set type?
+            set_type_void(n, s);
         }
         ast::Ntype::For(for_) => {
             // TODO: handle matching patterns
             eval_types(for_.in_expr.as_mut(), s)?;
             eval_types(for_.body.as_mut(), s)?;
+
+            set_type_void(n, s);
         }
         ast::Ntype::While(while_) => {
             let mut errors = Vec::new();
@@ -309,9 +369,13 @@ fn eval_types(n: &mut ast::Node, s: &mut Context) -> Result<(), Vec<ParsingError
             if errors.len() > 0 {
                 return Err(errors);
             }
+
+            set_type_void(n, s);
         }
         ast::Ntype::Loop(loop_) => {
             eval_types(loop_.body.as_mut(), s)?;
+
+            set_type_void(n, s);
         }
         ast::Ntype::Break => set_type(n, s, KfType::from_literal(EvaluatedType::Never), None),
         ast::Ntype::Continue => set_type(n, s, KfType::from_literal(EvaluatedType::Never), None),
@@ -320,19 +384,102 @@ fn eval_types(n: &mut ast::Node, s: &mut Context) -> Result<(), Vec<ParsingError
             for n in a {
                 eval_types(n, s)?;
             }
+
+            set_type_void(n, s);
         }
         ast::Ntype::FnDef(fndef) => {
             eval_types(fndef.body.as_mut(), s)?;
+            set_type_void(n, s);
         }
         ast::Ntype::FnCall(name, args) => {
-            // TODO: implement function lookup
-        }
-        ast::Ntype::VarDef(vardef) => match &mut vardef.expr {
-            Some(expr) => {
-                eval_types(expr.as_mut(), s)?;
+            // evaluate all expressions passed as arguments
+            for arg in args.iter_mut() {
+                eval_types(arg, s)?;
             }
-            None => {}
-        },
+            // do the validation
+            if let Some((args_def, ret_def)) = s.glob_functs.get(name) {
+                let mut errors = Vec::new();
+                if args.len() != args_def.len() {
+                    let passed = args.len();
+                    let expected = args_def.len();
+                    errors.push(ParsingError {
+                        msg: "incorrect arguments passed".into(),
+                        details: format!("passed {passed} arguments, expected: {expected}"),
+                        at: n.at,
+                    });
+                }
+                for (arg, arg_def) in args.iter().zip(args_def) {
+                    let arg_type = &get_side_node(arg, s)
+                        .expect("Side node should be calculated here")
+                        .eval_type
+                        .eval_type;
+                    let arg_type_dec = EvaluatedType::from_str(&arg_def.type_dec.typename);
+                    if let Some(arg_type_dec) = arg_type_dec {
+                        if *arg_type != arg_type_dec {
+                            // error
+                            errors.push(ParsingError {
+                                msg: "argument type mismatch".into(),
+                                details: format!("expected {arg_type_dec:?} but the expression evaluates to {arg_type:?}"),
+                                at: arg.at,
+                            });
+                        }
+                    } else {
+                        // TODO: not the best place for reporting this error
+                        let t = arg_def.type_dec.typename.clone();
+                        errors.push(ParsingError {
+                            msg: "type not recognised".into(),
+                            details: format!("type >{t}< is not defined"),
+                            at: arg.at,
+                        });
+                    }
+                }
+                // set return type
+                let ret_type = if ret_def.typename.len() == 0 {
+                    Some(EvaluatedType::Void)
+                } else {
+                    EvaluatedType::from_str(&ret_def.typename)
+                };
+                if let Some(ret_type) = ret_type {
+                    set_type(
+                        n,
+                        s,
+                        KfType {
+                            mutable: Some(false),
+                            eval_type: ret_type,
+                        },
+                        None,
+                    );
+                } else {
+                    // TODO: not the best place for reporting this error
+                    let t = ret_def.typename.clone();
+                    errors.push(ParsingError {
+                        msg: "function return value type not recognised".into(),
+                        details: format!("unknown type >{t}<"),
+                        at: n.at,
+                    });
+                }
+                if errors.len() > 0 {
+                    return Err(errors);
+                }
+            } else {
+                return Err(vec![ParsingError {
+                    msg: "function is not defined".into(),
+                    details: format!(
+                        "function >{name}< is not defined, check if you've made a typo"
+                    ),
+                    at: n.at,
+                }]);
+            }
+        }
+        ast::Ntype::VarDef(vardef) => {
+            match &mut vardef.expr {
+                Some(expr) => {
+                    eval_types(expr.as_mut(), s)?;
+                }
+                None => {}
+            }
+            set_type_void(n, s);
+        }
         // terminals
         ast::Ntype::String(ss) => {
             let ss = ss.clone();
@@ -399,6 +546,26 @@ fn set_type(n: &mut ast::Node, ctx: &mut Context, t: KfType, v: Option<Evaluated
     }
 }
 
+fn set_type_void(n: &mut ast::Node, ctx: &mut Context) {
+    let t = KfType {
+        mutable: None,
+        eval_type: EvaluatedType::Void,
+    };
+    match n.meta_idx {
+        Some(idx) => {
+            ctx.side_nodes[idx].eval_type = t;
+            ctx.side_nodes[idx].eval_val = None;
+        }
+        None => {
+            ctx.side_nodes.push(SideNode {
+                eval_type: t,
+                eval_val: None,
+            });
+            n.meta_idx = Some(ctx.side_nodes.len() - 1);
+        }
+    }
+}
+
 fn get_side_node<'a>(n: &'a ast::Node, ctx: &'a Context) -> Option<&'a SideNode> {
     if let Some(side_node) = n.meta_idx {
         Some(&ctx.side_nodes[side_node])
@@ -416,7 +583,7 @@ fn determine_type_for_a_b(
     let asn = get_side_node(a, ctx).expect("Side node should be calculated here!");
     let bsn = get_side_node(b, ctx).expect("Side node should be calculated here (2)");
 
-    println!("asn={:?}\nbsn={:?}", asn, bsn);
+    // println!("asn={:?}\nbsn={:?}", asn, bsn);
 
     // TODO: unfortunate names - figure out better namings
     let atype = asn.eval_type.eval_type.clone();
@@ -596,7 +763,13 @@ mod tests {
 
     #[test]
     fn test_positive_cases() {
-        let tcs = ["let v = 1 + 2;"];
+        let tcs = [
+            "let v = 1 + 2;",
+            "fn ab() {}",
+            "fn a() {} fn b() { a(); }",
+            "fn a() { a(); }",
+            "fn a() { let b: i32 = 0; if b == 0 {}}",
+        ];
         for c in tcs {
             let ast = parse_code(c).unwrap();
             let inter = InterRepr::from(ast);
@@ -615,7 +788,15 @@ mod tests {
 
     #[test]
     fn test_negative_cases() {
-        let tcs = [r#"fn a(){1 == "s"; }"#, "let v = 1 == \"srt\";"];
+        let tcs = [
+            r#"fn a(){1 == "s"; }"#,
+            "let v = 1 == \"srt\";",
+            "fn a() {} fn a() {}",
+            "fn 1a() {}",
+            "fn a() {} fn b() {aa();}",
+            "fn a(i: i32) {} fn b() {a(1, true);}",
+            "fn a(i: i32) {} fn b() {a(true);}",
+        ];
         for c in tcs {
             let ast = parse_code(c).unwrap();
             let inter = InterRepr::from(ast);
